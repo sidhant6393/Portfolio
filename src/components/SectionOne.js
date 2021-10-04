f9de25e7ed49b6fbc2ece2d1072107c8aa8bcf4a
Santosh Mane <santoshmane619@gmail.com>
2021-10-05 04:33:48 +0530

    Added fixed box

--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -1,18 +1,26 @@
 import styled from "styled-components";
 import { Navbar } from "./Navbar";
+import { breakPoint } from "../utils/breakPoints";
 
 const SectionContainer = styled.div`
     height: 100vh;
     position: relative;
+    border: 50px solid "yellow";
 
     .fixed-box {
         position: absolute;
         height: 100%;
-        width: 70%;
+        width: 67%;
         top: 0;
         right: 0;
         z-index: -1;
         background: ${(props) => props.theme.primary};
+        display: block;
+    }
+    @media (max-width: ${breakPoint.tablet}) {
+        .fixed-box {
+            display: none;
+        }
     }
 `;
 
