b1be3b27486d69941bb68d66344074629a06c0a9
Santosh Mane <santoshmane619@gmail.com>
2021-10-04 22:47:51 +0530

    cleared the default initial react template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1> Hi, All</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                repudiandae aliquam dolores laboriosam dignissimos! Aliquid odit
+                voluptas eos veritatis cum.
+            </p>
+        </div>
+    );
 }
 
 export default App;
