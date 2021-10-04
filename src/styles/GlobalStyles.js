37067eed409f59b869567dfafe17593c20dcbb09
Santosh Mane <santoshmane619@gmail.com>
2021-10-05 01:47:04 +0530

    added font

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Jaldi:wght@400;700&display=swap');
 
     *, *::before, *::after{
         margin: 0;
         padding: 0;
-        box-sizing: border-box,
+        box-sizing: border-box;
+        font-family: 'Jaldi', sans-serif;
     }
 
 `;
