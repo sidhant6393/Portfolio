67306648205b5042754a4cb4eae01658de5a92c5
Santosh Mane <santoshmane619@gmail.com>
2021-10-06 14:53:12 +0530

    added hr line

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,18 +1,42 @@
 import React from 'react'
-
 import styled from 'styled-components'
+
+const Hr = styled.div`
+    width: ${props => props.width};
+    display: inline-block;
+    height: 4px;
+    border-bottom: 1px solid #000;
+    margin-left: 10px;
+    
+
+
+`
 
 const AboutContainer = styled.section`
     height: 100vh;
     border: 1px solid #000;
+
+    & > .space{
+        margin: 6% 7%;
+        /* border: 1px solid #000; */
+    }
+
+    .about-me{
+        text-transform: uppercase;
+        display: flex;
+        align-items: center;
+
+    }
 `
+
+
 
 export const About = () => {
     return (
         <AboutContainer>
-
-
-            <h2 id="about-me">About Me</h2>
+            <div className="space">
+                <h2 id="about-me" className="about-me">ABOUT ME <Hr width={"82%"} /></h2>
+            </div>
 
         </AboutContainer>
     )
